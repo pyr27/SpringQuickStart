a3616f11188e3d874d7b97b92e56c0ecc3e8f037
Chocobone <yooho1030@naver.com>
2025-11-06 21:02:02 +0900

    hyunwoong initialize

---
+++ hyunwoongyu/src/main/java/com/example/hyunwoongyu/HyunwoongyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyunwoongyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunwoongyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyunwoongyuApplication.class, args);
+	}
+
+}
---
+++ hyunwoongyu/src/test/java/com/example/hyunwoongyu/HyunwoongyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyunwoongyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyunwoongyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
