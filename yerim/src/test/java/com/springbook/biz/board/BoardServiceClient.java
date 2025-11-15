555d84be1ded1fe58ed71aaf6b73878b230b2460
pyr27 <pyr8261@naver.com>
2025-11-15 22:33:42 +0900

    DAY02

---
+++ yerim/src/test/java/com/springbook/biz/board/BoardServiceClient.java
@@ -0,0 +1,28 @@
+package com.springbook.biz.board;
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BoardServiceClient {
+    public static void main(String[] args) {
+        AbstractApplicationContext container =
+                new GenericXmlApplicationContext("applicationContext.xml");
+
+        BoardService boardService = (BoardService) container.getBean("boardService");
+
+        BoardVO vo = new BoardVO();
+        vo.setTitle("Test");
+        vo.setWriter("user1");
+        vo.setContent("testconteq");
+        boardService.insertBoard(vo);
+
+        List<BoardVO> boardList = boardService.getBoardList(vo);
+        for (BoardVO board: boardList) {
+            System.out.println("---> " + board.toString());
+        }
+
+        container.close();
+    }
+}
